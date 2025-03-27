3b7b824d076bb08203752c776ead9c0d827caea6
Julian König <33655937+jkoenig134@users.noreply.github.com>
2025-03-27 17:05:04 +0100

    Prepare for zip build (#12)
    * chore: add debug dockerfile
    
    * chore: remove unused args
    
    * ci: publish npm
    
    * ci: use npm package in the final connector image
    
    * chore: disable provenance for now
    
    * chore: remove the MIT license
    
    * chore: remove docker testing pipeline

--- .ci/buildAndPush.sh
+++ .ci/buildAndPush.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -z "$VERSION" ]; then
-    echo "The environment variable 'VERSION' must be set."
+if [ -z "$SCHOOL_MODULE_VERSION" ]; then
+    echo "The environment variable 'SCHOOL_MODULE_VERSION' must be set."
     exit 1
 fi
 
-case "$VERSION" in
+case "$SCHOOL_MODULE_VERSION" in
 *-alpha*) BASE_TAG=alpha ;;
 *-beta*) BASE_TAG=beta ;;
 *-rc*) BASE_TAG=rc ;;
@@ -19,13 +19,13 @@ REPO="ghcr.io/nmshd/connector-school-module"
 
 TAGS="-t $REPO:$BUILD_NUMBER -t $REPO:$COMMIT_HASH"
 
-OUTPUT="$(DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect $REPO:${VERSION} 2>&1)" || true
+OUTPUT="$(DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect $REPO:${SCHOOL_MODULE_VERSION} 2>&1)" || true
 if [[ $OUTPUT =~ (no such manifest: ghcr.io/nmshd/connector-school-module:) ]] || [[ $OUTPUT == "manifest unknown" ]]; then # manifest not found -> push
-    echo "pushing tag '${BASE_TAG}' and '${VERSION}'"
+    echo "pushing tag '${BASE_TAG}' and '${SCHOOL_MODULE_VERSION}'"
 
-    TAGS="$TAGS -t $REPO:$BASE_TAG -t $REPO:$VERSION"
+    TAGS="$TAGS -t $REPO:$BASE_TAG -t $REPO:$SCHOOL_MODULE_VERSION"
 elif [[ $OUTPUT =~ (\{) ]]; then # manifest found -> ignore
-    echo "image '$VERSION' already exists"
+    echo "image '$SCHOOL_MODULE_VERSION' already exists"
 else # other error
     echo $OUTPUT
 fi
@@ -35,6 +35,4 @@ docker buildx build --push \
     --sbom=true \
     --platform linux/amd64,linux/arm64 \
     $TAGS \
-    --build-arg COMMIT_HASH=$COMMIT_HASH \
-    --build-arg BUILD_NUMBER=$BUILD_NUMBER \
-    --build-arg VERSION=$VERSION .
+    --build-arg SCHOOL_MODULE_VERSION=$SCHOOL_MODULE_VERSION .
--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,11 @@
-FROM node:23.10.0 AS module-builder
-
-WORKDIR /usr/app
-
-COPY . .
-
-RUN npm install && npx tsc
-
 FROM ghcr.io/nmshd/connector:6.18.0
+
+ARG SCHOOL_MODULE_VERSION
 
 LABEL org.opencontainers.image.source="https://github.com/nmshd/connector-school-module"
 
-COPY --from=module-builder /usr/app/dist /usr/app/custom-modules/school-module/dist
-COPY --from=module-builder /usr/app/package.json /usr/app/custom-modules/school-module/package.json
-COPY --from=module-builder /usr/app/package-lock.json /usr/app/custom-modules/school-module/package-lock.json
-
 USER root
 
-RUN cd /usr/app/custom-modules/school-module && npm install --omit=dev
+RUN npm install @nmshd/connector-school-module@${SCHOOL_MODULE_VERSION}
 
 USER node
