9c104a05c00da1efcd47b52f1641c84e95f8529b
Julian König <julian.koenig@js-soft.com>
2025-03-31 09:51:42 +0200

    feat: use jq to remove assetsLocation from the default config

--- .ci/buildConnectorZip.sh
+++ .ci/buildConnectorZip.sh
@@ -21,6 +21,7 @@ wget https://github.com/nmshd/connector/releases/download/${CONNECTOR_VERSION}/c
 unzip connector-${CONNECTOR_VERSION}.zip
 
 npm install --save-exact @nmshd/connector-school-module@${SCHOOL_MODULE_VERSION} --omit=dev
+cat ../bundled.config.json | jq -r 'del(.modules.school.assetsLocation)' >./bundled.config.json
 
 zip -r "../connector-${SCHOOL_MODULE_VERSION}.zip" dist package.json package-lock.json bundled.config.json
 
